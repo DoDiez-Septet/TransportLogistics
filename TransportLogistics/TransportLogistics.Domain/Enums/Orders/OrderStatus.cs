54a487649ce1ca7323fb0df3876ac4f8ef6bab1e
Alexandr Kutiev <antizboy1985@gmail.com>
2022-12-01 22:19:30 +0300

    Добавлен сервис для обработки заказов. Добавлена папка Enums для хранения общих перечислений (добален enum для OrderStatus - отслеживание заказов). Удалены ненужные проекты-слои в ApiGateway сервисе

---
+++ TransportLogistics/TransportLogistics.Domain/Enums/Orders/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace TransportLogistics.Domain.Enums.Orders;
+
+public enum OrderStatus
+{
+    Received = 1,
+    Confirmed = 2,
+    Loading = 3,
+    Loaded = 4,
+    Delivering = 5,
+    Delivered = 6,
+    WaitingForConsumer = 7,
+    Closed = 8
+}
