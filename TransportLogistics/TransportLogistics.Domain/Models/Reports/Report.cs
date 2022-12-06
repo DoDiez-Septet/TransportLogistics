f2398d7c4a7bf872d8fe98aed24d1333557a4d4c
Lyagaev <70940958+Lyagaev@users.noreply.github.com>
2022-12-06 15:31:10 +0500

    Добавил класс Report

---
+++ TransportLogistics/TransportLogistics.Domain/Models/Reports/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportLogistics.Domain.Models.Reports
+{
+    class Report
+    {
+        public int ReportId { get; set; }
+        public Datetime DateStart { get; set; }
+        public Datetime DateEnd { get; set; }
+        public List<Order> Oeders { get; set; }
+
+    }
+}
