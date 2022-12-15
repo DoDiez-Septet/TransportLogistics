848b31ae398ba948bae57649e918349b0c370cf7
Aleksandr Varaksin <avaraksin@ozon.ru>
2022-12-15 12:53:01 +0300

    OrderService

--- TransportLogistics/OrderService/Program.cs
+++ TransportLogistics/OrderService/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContextFactory<AppFactory>(
                 x => x.MigrationsAssembly("OrderService.DataAccess")));
 
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
