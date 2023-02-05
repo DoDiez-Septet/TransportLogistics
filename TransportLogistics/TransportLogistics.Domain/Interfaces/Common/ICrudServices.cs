287be7ff09c6beaa1c17ac266cf711385a80461a
Alexandr Kutiev <antizboy1985@gmail.com>
2023-02-06 00:51:08 +0300

    Создан сервис Users с минимальными возможностями (CRUD операциями) по добавлению пользователей

---
+++ TransportLogistics/TransportLogistics.Domain/Interfaces/Common/ICrudServices.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TransportLogistics.Domain.Interfaces.Common;
+
+public interface ICrudServices<T>
+{
+    public IEnumerable<T> GetAll();
+
+    public T GetById(string Id);
+
+    public string New(T entity);
+
+    public T Edit(string Id, T entity);
+
+    public void Delete(string Id);
+}
---
+++ TransportLogistics/UserService.DataAccess/ModelDb/UserDb.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TransportLogistics.Domain.Enums.Users;
+
+namespace UserService.DataAccess.ModelDb;
+
+/// <summary>
+/// Класс Пользователь, объект базы данных
+/// </summary>
+internal class UserDb
+{
+    /// <summary>
+    /// Идентификатор пользователя
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    [Required]
+    [MaxLength(100)]
+    [MinLength(3)]
+    public string FirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Фамилия пользователя
+    /// </summary>
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Email пользователя
+    /// </summary>
+    [Required]
+    [MaxLength(50)]
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Пароль пользователя
+    /// </summary>
+    [Required]
+    [MaxLength(30)]
+    public string Password { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Роль пользователя
+    /// </summary>
+    public UserRole Role { get; set; }
+}
