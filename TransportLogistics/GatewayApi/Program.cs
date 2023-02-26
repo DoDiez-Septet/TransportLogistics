d69cde9ce5da93eaacdbe87c5c647b0f4a629ed7
Alexandr Kutiev <antizboy1985@gmail.com>
2023-02-27 00:45:51 +0300

    Добавлена минмальная версия Web UI (Blazor WASM) для отображения списка Users. Добалено взаимодействие UI с сервисом UsersAPI через API Gateway

--- TransportLogistics/GatewayApi/Program.cs
+++ TransportLogistics/GatewayApi/Program.cs
@@ -1,25 +1,29 @@
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
+var _corsOriginPolicy = "CorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => 
+         options.AddPolicy(name: _corsOriginPolicy,
+                builder =>
+                    { 
+                       builder
+                          .WithOrigins("http://localhost:7086")
+                          .AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                    }));
+builder.Services.AddOcelot(builder.Configuration);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseCors("CorsPolicy");
+await app.UseOcelot();
 app.Run();
--- TransportLogistics/UserService.DataAccess/ApplicationDbContext.cs
+++ TransportLogistics/UserService.DataAccess/ApplicationDbContext.cs
@@ -14,17 +14,17 @@ internal class ApplicationDbContext : DbContext
 
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
-        if (!_isDatabaseCreated)
-        {
-            DataBaseRecreation();
+        //if (!_isDatabaseCreated)
+        //{
+        //    DataBaseRecreation();
 
-            _isDatabaseCreated = true;
-        }
+        //    _isDatabaseCreated = true;
+        //}
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        SeedDatabase(modelBuilder);
+        //SeedDatabase(modelBuilder);
     }
 
     private void DataBaseRecreation()
