814ec887c1620313f3add4da13871dd812b32d6e
Sayan <damdsayan@gmail.com>
2023-03-08 01:00:13 +0800

    Убрал старые классы, добавил новые

--- TransportLogistics/CustomerService.DataAccess/Repos/CustomerRepo.cs
+++ TransportLogistics/CustomerService.DataAccess/Repos/CustomerRepo.cs
@@ -1,17 +1,85 @@
-﻿using CustomerService.DataAccess.Models;
+﻿using AutoMapper;
+using CustomerService.DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransportLogistics.Domain.Interfaces.Customers.Repository;
+using TransportLogistics.Domain.Models.Customers;
 
 
 namespace CustomerService.DataAccess.Repos
 {
-    internal class CustomerRepo// : Repository<Customer, Guid>
+    internal class CustomerRepo : ICustomerRepository
     {
-        CustomerRepo(DbContext dbContext)// : base(dbContext) { }
-        { }
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CustomerRepo(ApplicationDbContext CustomerDbContext, IMapper mapper)
+        {
+            _context = CustomerDbContext;
+            _mapper = mapper;
+        }
+
+        public void Delete(string CustomerId)
+        {
+            if (Guid.TryParse(CustomerId, out Guid Id))
+            {
+                var CustomerToDelete = _context.Customers.Find(Id);
+                if (CustomerToDelete is not null)
+                {
+                    _context.Entry(CustomerToDelete).State = EntityState.Deleted;
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        public void Edit(string CustomerId, Customer Customer)
+        {
+            if (Guid.TryParse(CustomerId, out Guid Id))
+            {
+                if (_context.Customers.Find(Id) is CustomerDb CustomerInDb)
+                {
+                    CustomerInDb.Name = Customer.Name;
+                    CustomerInDb.ContactPerson = Customer.ContactPerson;
+                    CustomerInDb.Email = Customer.Email;
+                    CustomerInDb.PhoneNumber = Customer.PhoneNumber;
+                    CustomerInDb.Address = Customer.Address;
+
+                    _context.Entry(CustomerInDb).State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        public Customer? Get(string CustomerId)
+        {
+            if (Guid.TryParse(CustomerId, out Guid Id))
+            {
+                var CustomerDb = _context.Customers.FirstOrDefault(x => x.Id == Id);
+                return _mapper.Map<Customer?>(CustomerDb);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            var CustomersDb = _context.Customers.ToList();
+            return _mapper.Map<IReadOnlyCollection<Customer>>(CustomersDb);
+        }
+
+        public string New(Customer Customer)
+        {
+            var CustomerDb = _mapper.Map<CustomerDb>(Customer);
+            var result = _context.Customers.Add(CustomerDb);
+            _context.SaveChanges();
+
+            return result.Entity.Id.ToString();
+        }
     }
 }
---
+++ TransportLogistics/TransportLogistics.Domain/Interfaces/Customers/Repository/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using TransportLogistics.Domain.Interfaces.Common;
+using TransportLogistics.Domain.Models.Customers;
+
+namespace TransportLogistics.Domain.Interfaces.Customers.Repository;
+
+public interface ICustomerRepository : ICrudRepository<Customer>
+{
+}
